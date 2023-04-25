6830207a4aaa6be582ca365df8ca63d64543077b
gadaged <gadageddexp@gmail.com>
2023-04-25 18:13:46 +0400

    asgvbjnm
    sdfgh

--- front-end/js/log.js
+++ front-end/js/log.js
@@ -1,14 +1,17 @@
-const signInBtn = document.querySelector('.signin-btn');
-const signUpBtn = document.querySelector('.signup-btn');
-const formBox = document.querySelector('.form-box');
-const body = document.body;
 
+
+
+const body = document.body;
+const signUpBtn = document.getElementById('signup-btn');
 signUpBtn.addEventListener('click', function () {
+    const formBox = document.querySelector('.form-box');
     formBox.classList.add('active');
     body.classList.add('active');
+    
 });
-
-signInBtn.addEventListener('click', function() {
+let sig = document.getElementById('signin-btn')
+sig.addEventListener('click', function() {
+    const formBox = document.querySelector('.form-box');
     formBox.classList.remove('active');
     body.classList.remove('active');
 });
