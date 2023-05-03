399cc06cbb5479e7d2e82e0379dfe3442b54803b
belykh555 <multiworld177@gmail.com>
2023-05-03 15:29:28 +0400

    555

--- front-end/js/new.js
+++ front-end/js/new.js
@@ -1,7 +1,24 @@
 let male = document.getElementById("male")
+let malek = document.getElementById("malek")
+let info = document.querySelector(".info")
+let infos = document.querySelector(".infos")
 const males = () =>{
-fetch(`https://api.genderize.io/?name=${male.value}`)
+/*fetch(`https://api.genderize.io/?name=${male.value}`)
  .then(response => response.json())
  .then(data => { console.log(data.gender)})
+ .catch(error => console.error(error));
+}*/
+
+fetch(`https://api.ipify.org/?format=json`)
+ .then(response => response.json())
+ .then(data => { info.innerHTML =data.ip })
+ .catch(error => console.error(error));
+} 
+
+const maled = () =>{
+fetch(`https://ipinfo.io/${malek.value}/geo`)
+ .then(response => response.json())
+ .then(data => { info.innerHTML =data.city
+    infos.innerHTML =data.loc})
  .catch(error => console.error(error));
 }
